8337ceb6eb3b07241f48c215400c0f8d6c714904
Ben Burwell <bburwell1@gmail.com>
2012-03-30 11:11:52 -0400

    Added license

--- core/classes/database.class.php
+++ core/classes/database.class.php
@@ -10,9 +10,25 @@
 	@date 		2012-03-28 12:55:52 -0400 (Wed, 28 Mar 2012)
 	@author 	Ben Burwell <bburwell1@gmail.com>
 
-	Copyright (c) 2012 Ben Burwell <http://www.benburwell.com/>
-
 */
+
+################################################################################
+##                                                                            ##
+##  Copyright 2012 Ben Burwell                                                ##
+##                                                                            ##
+##  Licensed under the Apache License, Version 2.0 (the "License");           ##
+##  you may not use this file except in compliance with the License.          ##
+##  You may obtain a copy of the License at                                   ##
+##                                                                            ##
+##  http://www.apache.org/licenses/LICENSE-2.0                                ##
+##                                                                            ##
+##  Unless required by applicable law or agreed to in writing, software       ##
+##  distributed under the License is distributed on an "AS IS" BASIS,         ##
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  ##
+##  See the License for the specific language governing permissions and       ##
+##  limitations under the License.                                            ##
+##                                                                            ##
+################################################################################
 
 class Database {
 
